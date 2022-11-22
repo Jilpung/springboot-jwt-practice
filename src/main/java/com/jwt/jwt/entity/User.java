ac6262fc0fb2e370eead5186099dc79534b75f2c
Jilpung <86465970+Jilpung@users.noreply.github.com>
2022-11-22 17:17:12 +0900

    Initial commit

---
+++ src/main/java/com/jwt/jwt/entity/User.java
@@ -0,0 +1,42 @@
+package com.jwt.jwt.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import java.util.Set;
+
+import javax.persistence.*;
+
+// 데이터베이스의 테이블과 1:1 매핑되는 객체
+@Entity
+@Table(name = "`user`")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "username", length = 50, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "nickname", length = 50)
+    private String nickname;
+
+    @Column(name = "activated")
+    private boolean activated;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+}
